e68bd40b753ce817e269655692dbfebb64bf7b04
Lital Hen <litalh2702@gmail.com>
2021-05-12 01:47:14 +0300

    create pages and component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import { HashRouter, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import ManagementNavbar from './component/ManagementNavbar';
+import LogIn from './pages/LogIn';
+import LogOut from './pages/LogOut';
+import SignUp from './pages/SignUp';
+import HomePage from './pages/HomepAge';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HashRouter>
+        <Container>
+        <Route exact path={['/','/messages']}>
+          <ManagementNavbar></ManagementNavbar>
+        </Route>
+        <Route exact path='/login'>
+          <LogIn></LogIn>
+        </Route>
+        <Route exact path='/logout'>
+          <LogOut></LogOut>
+        </Route>
+        <Route exact path='/signup'>
+          <SignUp></SignUp>
+        </Route>
+        <Route exact path='/'>
+         <HomePage></HomePage>
+        </Route>
+        </Container>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/LogOut.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class LogOut extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+render(){
+    return(
+        <div></div>
+    )
+}
+    
+}
+
+export default LogOut
